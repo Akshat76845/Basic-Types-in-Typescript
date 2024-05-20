8c8c3ca72d2f06bee78dba467f2a1c2aae6187c4
Akshat <akshatgupta78886@gmail.com>
2024-05-21 00:45:38 +0530

    Arrays in Typescript

---
+++ src/a.ts
@@ -0,0 +1,10 @@
+"use strict";
+type NumberArr = number[];
+function maxValue(arr:NumberArr) {
+    let max = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+}
